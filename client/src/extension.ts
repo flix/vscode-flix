10d139afa4dfa68772ca4e68906f549927722663
Thomas Plougsgaard <thomas@plougsgaard.dk>
2020-09-02 20:46:43 +0200

    feat: Name the language "Flix" like other languages

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -30,8 +30,8 @@ export function activate(context: ExtensionContext) {
 
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
-    // Register the server for plain text documents
-    documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+    // Register the server for flix documents
+    documentSelector: [{ scheme: 'file', language: 'flix' }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
@@ -40,8 +40,8 @@ export function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'languageServerExample',
-    'Language Server Example',
+    'flixLanguageServer',
+    'Flix Language Server',
     serverOptions,
     clientOptions
   )
--- server/src/server.ts
+++ server/src/server.ts
@@ -98,7 +98,7 @@ connection.onDidChangeConfiguration(change => {
     documentSettings.clear()
   } else {
     globalSettings = <ExampleSettings>(
-      (change.settings.languageServerExample || defaultSettings)
+      (change.settings.flixLanguageServer || defaultSettings)
     )
   }
 
@@ -114,7 +114,7 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
   if (!result) {
     result = connection.workspace.getConfiguration({
       scopeUri: resource,
-      section: 'languageServerExample'
+      section: 'flixLanguageServer'
     })
     documentSettings.set(resource, result)
   }

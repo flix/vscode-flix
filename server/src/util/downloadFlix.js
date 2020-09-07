092a04a5c7f664ba3f1047fc098603d33a0213ec
Thomas Plougsgaard <thomas@plougsgaard.dk>
2020-09-07 21:35:34 +0200

    feat: Properly download compiler, but accept failure if it already exists

---
+++ server/src/util/downloadFlix.js
@@ -0,0 +1,36 @@
+const { https } = require('follow-redirects')
+const fs = require('fs')
+const path = require('path')
+
+const FLIX_URL = 'https://github.com/flix/flix/releases/download/v0.13.0/flix.jar'
+
+const downloadFile = ({ url, targetFile }) => new Promise((resolve, reject) => {
+  try {
+    https.get(url, response => {
+      response.pipe(targetFile)
+      response.on('close', resolve)
+      response.on('error', reject)
+    })
+  } catch (err) {
+    reject(err)
+  }
+})
+
+export default async function ({ targetPath }) {
+  if (!targetPath) {
+    throw 'Must be called with targetPath'
+  }
+  const filename = path.join(targetPath, 'flix.jar')
+  const flixExists = fs.existsSync(filename)
+  try {
+    const targetFile = fs.createWriteStream(filename)
+    return downloadFile({ url: FLIX_URL, targetFile })
+  } catch (err) {
+    if (!flixExists) {
+      throw err
+    } else {
+      // there is a flix available, so we just continue
+      console.error(err)
+    }
+  }
+}

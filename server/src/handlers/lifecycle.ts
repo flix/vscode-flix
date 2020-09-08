e4bea0ae337a4cf977cfc3d39ba2f4778af7bfd9
Thomas Plougsgaard <thomas@plougsgaard.dk>
2020-09-08 20:21:00 +0200

    docs: Remove outdated TODO

--- server/src/handlers/lifecycle.ts
+++ server/src/handlers/lifecycle.ts
@@ -7,17 +7,12 @@ export interface ReadyParams {
 /**
  * Runs when both client and server are ready.
  * 
- * TODO: init the flix engine
- * 
  * @param {String} obj.extensionPath - Install path of this extension.
  */
 export function handleReady ({ extensionPath }: ReadyParams) {
-	console.log('[handleReady]', extensionPath)
 	start({ extensionPath })
-	console.log('[handleReady] Engine Started?')
 }
 
 export function handleExit () {
-	console.log('[handleExit]')
 	stop()
 }

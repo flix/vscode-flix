fb2e649724d8029b7275b4ad4c3be9e5d41e33eb
Thomas Plougsgaard <thomas@plougsgaard.dk>
2020-09-10 20:59:40 +0200

    feat: Add jobs and queue

--- server/src/engine/jobs.ts
+++ server/src/engine/jobs.ts
@@ -1,11 +1,46 @@
-interface JobMap {
-  [key: string]: {
-    request: string
-  }
+const _ = require('lodash/fp')
+
+let counter = 0
+
+export interface Job {
+  request: string
+  uri: string
+  src?: string
 }
 
-export let jobs: JobMap = {
-  // '1': {
-  //   request: 'lsp/check'
-  // }
+export interface EnqueuedJob extends Job {
+  id: string
+}
+
+interface JobMap {
+  [id: string]: EnqueuedJob
+}
+
+export let jobs: JobMap = {}
+
+let queue: EnqueuedJob[] = []
+
+export function enqueue (job: Job) {
+  const id = `${counter++}`
+  const enqueuedJob = { ...job, id }
+  jobs[id] = enqueuedJob
+  queue.push(enqueuedJob)
+  console.warn(`[debug] added job`, enqueuedJob)
+}
+
+export function enqueueMany (jobs: [Job]) {
+  _.each(enqueue, jobs)
+}
+
+export function dequeue () {
+  if (_.isEmpty(queue)) {
+    return undefined
+  }
+  const first = _.first(queue)
+  queue.shift()
+  return first
+}
+
+export function getJob (id: string) {
+  return jobs[id]
 }

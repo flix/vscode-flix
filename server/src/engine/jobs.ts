8ecdc80e2a1def3db40adf74d3343b69da37c561
Thomas Plougsgaard <thomas@plougsgaard.dk>
2020-09-19 12:17:15 +0200

    feat: Add all request types with reference to LSP source

--- server/src/engine/jobs.ts
+++ server/src/engine/jobs.ts
@@ -1,38 +1,38 @@
-// https://github.com/flix/flix/blob/b4b9041cc89b8be04c173ce0b0f58a69e6993739/main/src/ca/uwaterloo/flix/api/lsp/LanguageServer.scala#L163
 
-// case JString("api/addUri") => Request.parseAddUri(json)
-// case JString("api/remUri") => Request.parseRemUri(json)
-// case JString("api/version") => Request.parseVersion(json)
-// case JString("api/shutdown") => Request.parseShutdown(json)
-
-// case JString("cmd/runBenchmarks") => Request.parseRunBenchmarks(json)
-// case JString("cmd/runMain") => Request.parseRunMain(json)
-// case JString("cmd/runTests") => Request.parseRunTests(json)
-
-// case JString("lsp/check") => Request.parseCheck(json)
-// case JString("lsp/codelens") => Request.parseCodelens(json)
-// case JString("lsp/complete") => Request.parseComplete(json)
-// case JString("lsp/hover") => Request.parseHover(json)
-// case JString("lsp/selectionRange") => Request.parseSelectionRange(json)
-// case JString("lsp/foldingRange") => Request.parseFoldingRange(json)
-// case JString("lsp/goto") => Request.parseGoto(json)
-// case JString("lsp/symbols") => Request.parseSymbols(json)
-// case JString("lsp/uses") => Request.parseUses(json)
-
-// case JString("pkg/benchmark") => Request.parsePackageBenchmark(json)
-// case JString("pkg/build") => Request.parsePackageBuild(json)
-// case JString("pkg/buildDoc") => Request.parsePackageBuildDoc(json)
-// case JString("pkg/buildJar") => Request.parsePackageBuildJar(json)
-// case JString("pkg/buildPkg") => Request.parsePackageBuildPkg(json)
-// case JString("pkg/init") => Request.parsePackageInit(json)
-// case JString("pkg/test") => Request.parsePackageTest(json)
-
+/**
+ * @enum
+ * 
+ * Request types matching that of the LSP implementation.
+ * 
+ * @see https://github.com/flix/flix/blob/b4b9041cc89b8be04c173ce0b0f58a69e6993739/main/src/ca/uwaterloo/flix/api/lsp/LanguageServer.scala#L163
+ */
 export enum Request {
-  lspCheck = 'lsp/check',
-  lspHover = 'lsp/hover',
-  lspGoto = 'lsp/goto',
   apiAddUri = 'api/addUri',
-  apiRemUri = 'api/remUri'
+  apiRemUri = 'api/remUri',
+  apiVersion = 'api/version', // TODO
+  apiShutdown = 'api/shutdown', // TODO
+
+  cmdRunBenchmarks = 'cmd/runBenchmarks', // TODO
+  cmdRunMain = 'cmd/runMain', // TODO
+  cmdRunTests = 'cmd/runTests', // TODO
+
+  lspCheck = 'lsp/check',
+  lspCodelens = 'lsp/codelens', // TODO
+  lspComplete = 'lsp/complete', // TODO
+  lspHover = 'lsp/hover',
+  lspSelectionRange = 'lsp/selectionRange', // TODO
+  lspFoldingRange = 'lsp/foldingRange', // TODO
+  lspGoto = 'lsp/goto',
+  lspSymbols = 'lsp/symbols', // TODO
+  lspUses = 'lsp/uses', // TODO
+
+  pkgBenchmark = 'pkg/benchmark', // TODO
+  pkgBuild = 'pkg/build', // TODO
+  pkgBuildDoc = 'pkg/buildDoc', // TODO
+  pkgBuildJar = 'pkg/buildJar', // TODO
+  pkgBuildPkg = 'pkg/buildPkg', // TODO
+  pkgInit = 'pkg/init', // TODO
+  pkgTest = 'pkg/test' // TODO
 }
 
 export interface Position {

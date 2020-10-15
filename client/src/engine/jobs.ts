7ad5cd6d9967803e44e1ee0ebfe8385cc2a929a9
Thomas Plougsgaard <thomas@plougsgaard.dk>
2020-10-15 19:42:06 +0200

    feat: Add new internal job type to show finished jobs

--- client/src/engine/jobs.ts
+++ client/src/engine/jobs.ts
@@ -51,5 +51,6 @@ export enum Request {
   internalDownloadLatest = 'ext/downloadLatest', // Internal Extension Request
   internalReady = 'ext/ready', // Internal Extension Request
   internalMessage = 'ext/message', // Internal Extension Request
-  internalError = 'ext/error' // Internal Extension Request
+  internalError = 'ext/error', // Internal Extension Request
+  internalFinishedJob = 'ext/finished' // Internal Extension Request
 }

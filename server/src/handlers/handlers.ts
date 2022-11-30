a557431bc856e922894f46b2f7e514758e17c5c5
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-11-30 16:51:15 +0100

    feat: add ? to trigger chars

--- server/src/handlers/handlers.ts
+++ server/src/handlers/handlers.ts
@@ -42,7 +42,7 @@ export function handleInitialize (_params: InitializeParams) {
       textDocumentSync: TextDocumentSyncKind.Incremental,
       documentHighlightProvider: true,
       completionProvider: {
-        triggerCharacters: [".", "/"]
+        triggerCharacters: [".", "/", "?"]
       }, 
       hoverProvider: true,
       inlayHintProvider: true,

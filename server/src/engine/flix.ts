c12f7d055d0ccf926cf93440ada65736c2e56cc4
mR4smussen <m.rasmussen.uni@gmail.com>
2023-02-09 15:29:05 +0100

    fix: changed the error message for when we don't find a major java version

--- server/src/engine/flix.ts
+++ server/src/engine/flix.ts
@@ -107,6 +107,13 @@ export async function start (input: StartEngineInput) {
 
   // Check for valid Java version
   const { majorVersion, versionString } = await javaVersion(extensionPath)
+  if (majorVersion == 0) {
+    // This happends when we are not able to run a java statement or get a java version
+    sendNotification(jobs.Request.internalError, 
+        "Unable to find java on PATH. Please check that Java is correctly installed and on your PATH")
+    return
+  }
+  
   if (majorVersion < 11) {
     sendNotification(jobs.Request.internalError, `Flix requires Java 11 or later. Found "${versionString}".`)
     return

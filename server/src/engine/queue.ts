7c9d4b8f5b573fb42df02eaec499cb74e7f2a413
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-01-30 17:36:17 +0100

    chore: revert reimplementation of `makeEnqueueDebounced()`

--- server/src/engine/queue.ts
+++ server/src/engine/queue.ts
@@ -21,6 +21,7 @@ import { fileURLToPath } from 'url'
 import { sendNotification } from '../server'
 import { USER_MESSAGE } from '../util/userMessages'
 
+import * as _ from 'lodash/fp'
 import * as fs from 'fs'
 
 let jobCounter = 0
@@ -67,37 +68,9 @@ function handleEnqueue() {
   startQueue()
 }
 
-function makeEnqueueDebounced() {
-  let coolDownTimer: NodeJS.Timeout | null = null
-  let waiting = false
-
-  function startCooldown() {
-    coolDownTimer = setTimeout(endCooldown, engine.compileOnChangeDelay())
-  }
-
-  function endCooldown() {
-    coolDownTimer = null
-
-    // If there was a call during cool-down, handle it now
-    if (waiting) {
-      waiting = false
-      handleEnqueue()
-      startCooldown()
-    }
-  }
-
-  return () => {
-    // If we're still in cool-down,
-    // register that we want to be called at the end of the cool-down
-    if (coolDownTimer !== null) {
-      waiting = true
-      return
-    }
-
-    // If we're not in cool down, call immediately and start cool-down
-    handleEnqueue()
-    startCooldown()
-  }
+function makeEnqueueDebounced(): () => void {
+  // @ts-ignore
+  return _.debounce(engine.compileOnChangeDelay(), handleEnqueue, { leading: true, trailing: true })
 }
 
 export function resetEnqueueDebounced() {

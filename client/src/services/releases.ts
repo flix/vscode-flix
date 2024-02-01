d9f7b21438c4889ce1f8d6fc82345b0e337afd60
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2024-02-01 06:05:09 +0100

    Revert "refactor: replace node-fetch-commonjs with native fetch API (#336)"
    This reverts commit 893321b926f36dc28b2d521ded5b7dd4306a7d49.

--- client/src/services/releases.ts
+++ client/src/services/releases.ts
@@ -2,6 +2,8 @@
 // https://github.com/rust-analyzer/rust-analyzer/blob/master/editors/code/src/net.ts
 // The code in rust-analyzer is released under the same licence as this project.
 
+// Replace with `import fetch from "node-fetch"` once this is fixed in rollup:
+// https://github.com/rollup/plugins/issues/491
 import * as vscode from 'vscode'
 import * as stream from 'stream'
 import * as crypto from 'crypto'
@@ -11,6 +13,7 @@ import * as util from 'util'
 import * as path from 'path'
 import { strict as nativeAssert } from 'assert'
 import * as _ from 'lodash'
+const fetch = require('node-fetch-commonjs')
 
 const pipeline = util.promisify(stream.pipeline)
 
@@ -58,8 +61,8 @@ export async function fetchRelease(
     throw new Error(`Got response ${response.status} when trying to fetch ` + `release info for ${releaseTag} release`)
   }
 
-  // We skip runtime type checks for simplicity (here we cast from `unknown` to `GithubRelease`)
-  const release = (await response.json()) as GithubRelease
+  // We skip runtime type checks for simplicity (here we cast from `any` to `GithubRelease`)
+  const release: GithubRelease = await response.json()
   const flixRelease: FlixRelease = {
     url: _.get(release, 'url'),
     id: _.get(release, 'id'),
@@ -198,18 +201,14 @@ async function downloadFile(
 
   console.warn('Downloading file of', totalBytes, 'bytes size from', url, 'to', destFilePath)
 
-  // The fetch API uses the ReadableStream from 'stream/web'
-  // Convert it the the native one
-  const bodyStream = stream.Readable.fromWeb(res.body)
-
   let readBytes = 0
-  bodyStream.on('data', (chunk: Buffer) => {
+  res.body.on('data', (chunk: Buffer) => {
     readBytes += chunk.length
     onProgress(readBytes, totalBytes)
   })
 
   const destFileStream = fs.createWriteStream(destFilePath, { mode })
-  const srcStream = gunzip ? bodyStream.pipe(zlib.createGunzip()) : bodyStream
+  const srcStream = gunzip ? res.body.pipe(zlib.createGunzip()) : res.body
 
   await pipeline(srcStream, destFileStream)
 

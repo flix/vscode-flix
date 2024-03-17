7c7aa4c14b5f90fae4b05efd707b724496c5b398
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-03-17 16:31:38 +0100

    chore: also run tests for minimum supported version

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require('@vscode/test-cli')
 
+/**
+ * The desired version of VS Code can be optionally passed as an environment variable, for example:
+ * - `stable`
+ * - `1.80.0`
+ */
+let version = process.env.VSCODE_VERSION
+
 module.exports = defineConfig({
+  version,
   files: ['test/out/**/*.test.js'],
   workspaceFolder: 'test/testWorkspace',
 

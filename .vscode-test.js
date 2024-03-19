826057c161a28fd0766dc27016577c4c5761e37a
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2024-03-19 08:37:55 +0100

    test: run test suite in CI (#371)

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require('@vscode/test-cli')
 
+/**
+ * The desired version of VS Code can be optionally passed as an environment variable, for example:
+ * - `stable`
+ * - `1.80.0`
+ */
+let version = process.env.VSCODE_VERSION
+
 module.exports = defineConfig({
+  version,
   files: ['test/out/**/*.test.js'],
   workspaceFolder: 'test/testWorkspace',
 
   mocha: {
     // Downloading compiler takes a long time
     timeout: 120000,
+    color: true,
   },
 })

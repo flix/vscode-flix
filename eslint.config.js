fb55342ca0bbe2476513a77068c6c9d867b1a532
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2025-02-23 09:59:21 +0100

    chore: upgrade dependencies (#481)

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    ignores: [
+      '.vscode',
+      '.vscode-test',
+      'node_modules',
+      'client/node_modules',
+      'client/out',
+      'server/node_modules',
+      'server/out',
+      'test',
+    ],
+  },
+]

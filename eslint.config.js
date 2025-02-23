7be751d8b0af983b47fe0d57fb6a3d627d4cd2a9
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2025-02-23 09:51:21 +0100

    .

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    "ignores": [
+      ".vscode",
+      ".vscode-test",
+      "node_modules",
+      "client/node_modules",
+      "client/out",
+      "server/node_modules",
+      "server/out",
+      "test"
+    ]
+}
+];
